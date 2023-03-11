4fc5aaeefb3b1b1c80f113ce95175d359187628b
azeezWebsultanate <azeez@websultanate.com>
2023-03-11 18:20:25 +0530

    changes

---
+++ src/config/axios.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { server } from './keys';
+import Cookies from 'js-cookie';
+
+export const getToken = () => {
+  const tokenData = Cookies.get('token') ? Cookies.get('token') : {};
+
+  return tokenData;
+};
+
+export const getAuthorizationHeader = () => `Bearer ${getToken()}`;
+
+export const marketPlaceInstance = () =>
+  axios.create({
+    baseURL: `${server}/api`,
+    headers: { Authorization: getAuthorizationHeader() },
+  });
