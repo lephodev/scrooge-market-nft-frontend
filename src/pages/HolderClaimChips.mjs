eeb8ea1068f670e5be6dc7e851ff3c1be623d4f6
RizwanAhmad-Dev <rizwan@websultanate.com>
2023-03-14 14:50:03 +0530

    og contract address update

--- src/pages/HolderClaimChips.mjs
+++ src/pages/HolderClaimChips.mjs
@@ -41,7 +41,7 @@ function HolderClaimChips() {
     };
 
     async function getCoinGeckoData() {
-      await fetch('https://api.coingecko.com/api/v3/coins/binance-smart-chain/contract/0xfa1ba18067ac6884fb26e329e60273488a247fc3')
+      await fetch(`https://api.coingecko.com/api/v3/coins/binance-smart-chain/contract/${process.env.REACT_APP_OGCONTRACT_ADDRESS}`)
         .then(response => response.json())
         .then((data) => {
             const current_price = data.market_data.current_price.usd;
