b201f49c3239373dccf73cc9f219fb18ef93c11f
azeezWebsultanate <azeez@websultanate.com>
2023-03-16 17:20:26 +0530

    feature

--- src/components/AffiliateLeaderboard.mjs
+++ src/components/AffiliateLeaderboard.mjs
@@ -1,206 +1,239 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Axios from 'axios';
 
-export default function AffiliateLeaderboard(){
-    let user_id;
-    const [user, setUser]=useState([]);
-    const [leaderboard, setLeaderboard]=useState([]);
-    const [showLeaderboards, setShowLeaderboards]=useState(true);
-    const [leaderboardByCount, setLeaderboardByCount]=useState([]);
-    const [leaderboardByTokens, setLeaderboardByTokens]=useState([]);
-    const [leaderboardHottestNewcomers, setLeaderboardHottestNewcomers]=useState([]);
-    const [leaderboardMonthlyMovers, setLeaderboardMonthlyMovers]=useState([]);
-    const [leaderboardTopSales, setLeaderboardTopSales]=useState([]);
-    const [leaderboardTopRegisters, setLeaderboardTopRegisters]=useState([]);
+export default function AffiliateLeaderboard() {
+  let user_id;
+  const [user, setUser] = useState([]);
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [showLeaderboards, setShowLeaderboards] = useState(true);
+  const [leaderboardByCount, setLeaderboardByCount] = useState([]);
+  const [leaderboardByTokens, setLeaderboardByTokens] = useState([]);
+  const [leaderboardHottestNewcomers, setLeaderboardHottestNewcomers] =
+    useState([]);
+  const [leaderboardMonthlyMovers, setLeaderboardMonthlyMovers] = useState([]);
+  const [leaderboardTopSales, setLeaderboardTopSales] = useState([]);
+  const [leaderboardTopRegisters, setLeaderboardTopRegisters] = useState([]);
 
-    async function getAffLeadersByCount(limit=10, days=0) {
-        try {
-            Axios.get(`https://34.237.237.45:9001/api/getAffLeadersByCount/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByCount: ", data.data);
-                setLeaderboardByCount(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
+  async function getAffLeadersByCount(limit = 10, days = 0) {
+    Axios.get(`http://localhost:9001/api/getAffLeadersByCount/${limit}/${days}`)
+      .then((data) => {
+        console.log('getAffLeadersByCount: ', data);
+        setLeaderboardByCount(data.data);
+        return data.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  async function getAffLeadersByTokens(limit = 10, days = 0) {
+    Axios.get(
+      `http://localhost:9001/api/getAffLeadersByTokens/${limit}/${days}`
+    )
+      .then((data) => {
+        //console.log("getAffLeadersByTokens: ", data.data);
+        console.log('getAffLeadersBytoken', data);
+        setLeaderboardByTokens(data.data);
+        return data.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  async function getAffLeadersHottestNewcomers(limit = 10, days = 7) {
+    try {
+      Axios.get(
+        `http://localhost:9001/api/getAffLeadersByTokens/${limit}/${days}`
+      ).then((data) => {
+        //console.log("getAffLeadersByTokens: ", data.data);
+        setLeaderboardHottestNewcomers(data.data);
+        return data.data;
+      });
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    async function getAffLeadersByTokens(limit=10, days=0) {
-        try {
-            Axios.get(`https://34.237.237.45:9001/api/getAffLeadersByTokens/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                setLeaderboardByTokens(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
+  async function getAffLeadersMonthlyMovers(limit = 10, days = 30) {
+    try {
+      Axios.get(
+        `http://localhost:9001/api/getAffLeadersByTokens/${limit}/${days}`
+      ).then((data) => {
+        //console.log("getAffLeadersByTokens: ", data.data);
+        setLeaderboardMonthlyMovers(data.data);
+        return data.data;
+      });
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    async function getAffLeadersHottestNewcomers(limit=10, days=7) {
-        try {
-            Axios.get(`https://34.237.237.45:9001/api/getAffLeadersByTokens/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                setLeaderboardHottestNewcomers(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
+  async function getAffLeadersTopSales(type = 'order', limit = 10, days = 0) {
+    try {
+      Axios.get(
+        `http://localhost:9001/api/getAffLeadersByType/${type}/${limit}/${days}`
+      ).then((data) => {
+        //console.log("getAffLeadersByTokens: ", data.data);
+        setLeaderboardTopSales(data.data);
+        return data.data;
+      });
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    async function getAffLeadersMonthlyMovers(limit=10, days=30) {
-        try {
-            Axios.get(`https://34.237.237.45:9001/api/getAffLeadersByTokens/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                setLeaderboardMonthlyMovers(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
+  async function getAffLeadersTopRegisters(
+    type = 'register',
+    limit = 10,
+    days = 0
+  ) {
+    try {
+      Axios.get(
+        `http://localhost:9001/api/getAffLeadersByType/${type}/${limit}/${days}`
+      ).then((data) => {
+        //console.log("getAffLeadersByTokens: ", data.data);
+        setLeaderboardTopRegisters(data.data);
+        return data.data;
+      });
+    } catch (err) {
+      console.error(err);
     }
+  }
 
+  useEffect(() => {
+    //checkToken();
+    getAffLeadersByCount();
+    getAffLeadersByTokens();
+    getAffLeadersHottestNewcomers();
+    getAffLeadersMonthlyMovers();
+    getAffLeadersTopRegisters();
+    getAffLeadersTopSales();
+  }, []);
 
-
-
-    async function getAffLeadersTopSales(type='order', limit=10, days=0) {
-        try {
-            Axios.get(`https://34.237.237.45:9001/api/getAffLeadersByType/${type}/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                setLeaderboardTopSales(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
-    }
-
-    async function getAffLeadersTopRegisters(type='register', limit=10, days=0) {
-        try {
-            Axios.get(`https://34.237.237.45:9001/api/getAffLeadersByType/${type}/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                setLeaderboardTopRegisters(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
-    }
-
-    
-    
-    useEffect(() => {
-        //checkToken();
-        getAffLeadersByCount();
-        getAffLeadersByTokens();
-        getAffLeadersHottestNewcomers();
-        getAffLeadersMonthlyMovers();
-        getAffLeadersTopRegisters();
-        getAffLeadersTopSales();
-      }, []);
-
-
-    return (<>
-    {(showLeaderboards)?(
-
-    
-    <div className="bordered-section">
-        <div className="flex-row-no-margin">
-            {(leaderboardByCount.length > 0)?(<>
+  return (
+    <>
+      {showLeaderboards ? (
+        <div className='bordered-section'>
+          <div className='flex-row-no-margin'>
+            {leaderboardByCount.length > 0 ? (
+              <>
                 <div className='leaderboard-card-div'>
-                    <div className="pageTitle" style={{marginBottom: "20px"}}>
-                        <h1>Affiliate Sales Leaders</h1>
+                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                    <h1>Affiliate Sales Leaders</h1>
+                  </div>
+                  {leaderboardByCount.map((lead, index) => (
+                    <div className='leaderboard-row' key={lead._id}>
+                      <div>{index + 1}.</div>
+                      <div>Affiliate ID: {lead._id}</div>
+                      <div># of Sales: {lead.count}</div>
                     </div>
-                    {leaderboardByCount.map((lead, index) => (
-                        <div className='leaderboard-row' key={lead._id}>
-                            <div>{index+1}.</div>
-                            <div>Affiliate ID: {lead._id}</div>
-                            <div># of Sales: {lead.count}</div>
-                        </div>
-                    ))}
+                  ))}
                 </div>
-            </>):(<></>)}
+              </>
+            ) : (
+              <></>
+            )}
 
-            {(leaderboardByTokens.length > 0)?(<>
+            {leaderboardByTokens.length > 0 ? (
+              <>
                 <div className='leaderboard-card-div'>
-                    <div className="pageTitle" style={{marginBottom: "20px"}}>
-                        <h1>Affiliate Tokens Leaders</h1>
+                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                    <h1>Affiliate Tokens Leaders</h1>
+                  </div>
+                  {leaderboardByTokens.map((lead, index) => (
+                    <div className='leaderboard-row' key={lead._id}>
+                      <div>{index + 1}.</div>
+                      <div>Affiliate ID: {lead._id}</div>
+                      <div>Earned: {lead.totalCommission} Tokens</div>
                     </div>
-                    {leaderboardByTokens.map((lead, index) => (
-                        <div className='leaderboard-row' key={lead._id}>
-                            <div>{index+1}.</div>
-                            <div>Affiliate ID: {lead._id}</div>
-                            <div>Earned: {lead.totalCommission} Tokens</div>
-                        </div>
-                    ))}
+                  ))}
                 </div>
-            </>):(<></>)}
+              </>
+            ) : (
+              <></>
+            )}
 
-            {(leaderboardHottestNewcomers.length > 0)?(<>
+            {leaderboardHottestNewcomers.length > 0 ? (
+              <>
                 <div className='leaderboard-card-div'>
-                    <div className="pageTitle" style={{marginBottom: "20px"}}>
-                        <h1>Hottest Newcomers</h1>
+                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                    <h1>Hottest Newcomers</h1>
+                  </div>
+                  {leaderboardHottestNewcomers.map((lead, index) => (
+                    <div className='leaderboard-row' key={lead._id}>
+                      <div>{index + 1}.</div>
+                      <div>Affiliate ID: {lead._id}</div>
+                      <div>Earned: {lead.totalCommission} Tokens</div>
                     </div>
-                    {leaderboardHottestNewcomers.map((lead, index) => (
-                        <div className='leaderboard-row' key={lead._id}>
-                            <div>{index+1}.</div>
-                            <div>Affiliate ID: {lead._id}</div>
-                            <div>Earned: {lead.totalCommission} Tokens</div>
-                        </div>
-                    ))}
+                  ))}
                 </div>
-            </>):(<></>)}
+              </>
+            ) : (
+              <></>
+            )}
 
-            {(leaderboardMonthlyMovers.length > 0)?(<>
+            {leaderboardMonthlyMovers.length > 0 ? (
+              <>
                 <div className='leaderboard-card-div'>
-                    <div className="pageTitle" style={{marginBottom: "20px"}}>
-                        <h1>Monthly Movers</h1>
+                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                    <h1>Monthly Movers</h1>
+                  </div>
+                  {leaderboardMonthlyMovers.map((lead, index) => (
+                    <div className='leaderboard-row' key={lead._id}>
+                      <div>{index + 1}.</div>
+                      <div>Affiliate ID: {lead._id}</div>
+                      <div>Earned: {lead.totalCommission} Tokens</div>
                     </div>
-                    {leaderboardMonthlyMovers.map((lead, index) => (
-                        <div className='leaderboard-row' key={lead._id}>
-                            <div>{index+1}.</div>
-                            <div>Affiliate ID: {lead._id}</div>
-                            <div>Earned: {lead.totalCommission} Tokens</div>
-                        </div>
-                    ))}
+                  ))}
                 </div>
-            </>):(<></>)}
+              </>
+            ) : (
+              <></>
+            )}
 
-            {(leaderboardTopRegisters.length > 0)?(<>
+            {leaderboardTopRegisters.length > 0 ? (
+              <>
                 <div className='leaderboard-card-div'>
-                    <div className="pageTitle" style={{marginBottom: "20px"}}>
-                        <h1>Top User Referrals</h1>
+                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                    <h1>Top User Referrals</h1>
+                  </div>
+                  {leaderboardTopRegisters.map((lead, index) => (
+                    <div className='leaderboard-row' key={lead._id}>
+                      <div>{index + 1}.</div>
+                      <div>Affiliate ID: {lead._id}</div>
+                      <div>Earned: {lead.totalCommission} Tokens</div>
                     </div>
-                    {leaderboardTopRegisters.map((lead, index) => (
-                        <div className='leaderboard-row' key={lead._id}>
-                            <div>{index+1}.</div>
-                            <div>Affiliate ID: {lead._id}</div>
-                            <div>Earned: {lead.totalCommission} Tokens</div>
-                        </div>
-                    ))}
+                  ))}
                 </div>
-            </>):(<></>)}
+              </>
+            ) : (
+              <></>
+            )}
 
-            {(leaderboardTopSales.length > 0)?(<>
+            {leaderboardTopSales.length > 0 ? (
+              <>
                 <div className='leaderboard-card-div'>
-                    <div className="pageTitle" style={{marginBottom: "20px"}}>
-                        <h1>Top Product Sales</h1>
+                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                    <h1>Top Product Sales</h1>
+                  </div>
+                  {leaderboardTopSales.map((lead, index) => (
+                    <div className='leaderboard-row' key={lead._id}>
+                      <div>{index + 1}.</div>
+                      <div>Affiliate ID: {lead._id}</div>
+                      <div>Earned: {lead.totalCommission} Tokens</div>
                     </div>
-                    {leaderboardTopSales.map((lead, index) => (
-                        <div className='leaderboard-row' key={lead._id}>
-                            <div>{index+1}.</div>
-                            <div>Affiliate ID: {lead._id}</div>
-                            <div>Earned: {lead.totalCommission} Tokens</div>
-                        </div>
-                    ))}
+                  ))}
                 </div>
-            </>):(<></>)}
-
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
-        
-
-    </div>
-    ):(<></>)}
-    </>);
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
--- src/scripts/getAffilateUser.mjs
+++ src/scripts/getAffilateUser.mjs
@@ -1,37 +1,46 @@
 import Axios from 'axios';
 
 export default async function getAffiliateUser(user_id) {
-    let affilateUser;
-    try {
-        await Axios.get(`https://34.237.237.45:9001/api/getAffiliateUser/${user_id}`).then((data)=>{
-            affilateUser = data.data;
-            //return data.data;
-        });
-    } catch (err) {
-        console.error(err);
-    };
-    //console.log("data: ", affilateUser); 
-    return affilateUser;
-};
+  let affilateUser;
+  try {
+    await Axios.get(
+      `http://localhost:9001/api/getAffiliateUser/${user_id}`
+    ).then((data) => {
+      console.log(data);
+      affilateUser = data.data;
+      //return data.data;
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  //console.log("data: ", affilateUser);
+  return affilateUser;
+}
 
 export async function createAffiliateUser(user_id) {
-    
-    const createAffiliateUserData = async () => {
-        let resp;
-        try {
-            const res = await Axios.get('https://geolocation-db.com/json/');
-            await Axios.get(`https://34.237.237.45:9001/api/createAffiliateUser/${user_id}/${res.data.IPv4}`).then((data)=>{
-                //console.log("data: ", data.data); 
-                resp = data.data;
-            });
-        } catch (err) {
-            console.error(err);
-            resp = 'Error';
-        };
-        return resp;
-    };
-    return createAffiliateUserData();
-};
+  const createAffiliateUserData = async () => {
+    let resp;
+    try {
+      const res = await Axios.get('https://geolocation-db.com/json/');
+      const data = await Axios.get(
+        `http://localhost:9001/api/createAffiliateUser/${user_id}/${res.data.IPv4}`
+      );
+      //console.log("data: ", data.data);
+      if (data.success) {
+        console.log('if', data);
+        return data;
+      } else {
+        console.log('else', data);
+        return data;
+      }
+    } catch (err) {
+      console.error('err', err);
+      resp = 'Error';
+    }
+    return resp;
+  };
+  return createAffiliateUserData();
+}
 
 /*export async function getAffiliateCard(user_id) {
     let affiliate;
