7d6aebfb68ce31cab44b7dcb1f3cfd10a7075e8f
azeezWebsultanate <azeez@websultanate.com>
2023-03-20 16:40:37 +0530

    earn free token page changes

--- src/components/AffiliateLeaderboard.mjs
+++ src/components/AffiliateLeaderboard.mjs
@@ -1,240 +1,287 @@
-import {useContext, useEffect, useState} from 'react';
-import Axios from 'axios';
-import AuthContext from '../context/authContext.ts';
-import { marketPlaceInstance } from '../config/axios.js';
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/authContext.ts";
+import { marketPlaceInstance } from "../config/axios.js";
 
-export default function AffiliateLeaderboard(){
+export default function AffiliateLeaderboard() {
+  const { user } = useContext(AuthContext);
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [showLeaderboards, setShowLeaderboards] = useState(true);
+  const [leaderboardByCount, setLeaderboardByCount] = useState([]);
+  const [leaderboardByTokens, setLeaderboardByTokens] = useState([]);
+  const [leaderboardHottestNewcomers, setLeaderboardHottestNewcomers] =
+    useState([]);
+  const [leaderboardMonthlyMovers, setLeaderboardMonthlyMovers] = useState([]);
+  const [leaderboardTopSales, setLeaderboardTopSales] = useState([]);
+  const [leaderboardTopRegisters, setLeaderboardTopRegisters] = useState([]);
 
-   const { user } = useContext(AuthContext);
-    const [leaderboard, setLeaderboard]=useState([]);
-    const [showLeaderboards, setShowLeaderboards]=useState(true);
-    const [leaderboardByCount, setLeaderboardByCount]=useState([]);
-    const [leaderboardByTokens, setLeaderboardByTokens]=useState([]);
-    const [leaderboardHottestNewcomers, setLeaderboardHottestNewcomers]=useState([]);
-    const [leaderboardMonthlyMovers, setLeaderboardMonthlyMovers]=useState([]);
-    const [leaderboardTopSales, setLeaderboardTopSales]=useState([]);
-    const [leaderboardTopRegisters, setLeaderboardTopRegisters]=useState([]);
+  const getAffLeadersByCount = async (limit = 10, days = 0) => {
+    try {
+      const data = await marketPlaceInstance().get(
+        `/getAffLeadersByCount/${limit}/${days}`
+      );
+      if (data.data.success) {
+        setLeaderboardByCount(data.data.data);
+      } else {
+        setLeaderboardByCount(data.data.message);
+      }
+    } catch (err) {
+      setLeaderboardByCount("Error");
+      console.error("bycount", err);
+    }
+  };
 
-    async function getAffLeadersByCount(limit=10, days=0) {
-        try {
-            marketPlaceInstance().get(`/getAffLeadersByCount/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByCount: ", data.data);
-                if(typeof data.data !== "string")
-                setLeaderboardByCount(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
+  const getAffLeadersByTokens = async (limit = 10, days = 0) => {
+    try {
+      const data = await marketPlaceInstance().get(
+        `/getAffLeadersByTokens/${limit}/${days}`
+      );
+      if (data.data.success) {
+        setLeaderboardByTokens(data.data.data);
+      } else {
+        setLeaderboardByTokens(data.data.message);
+      }
+    } catch (err) {
+      setLeaderboardByTokens("Error");
+      console.error("error", err);
+    }
+  };
+
+  const getAffLeadersHottestNewcomers = async (limit = 10, days = 7) => {
+    try {
+      const data = await marketPlaceInstance().get(
+        `/getAffLeadersByTokens/${limit}/${days}`
+      );
+
+      //console.log("getAffLeadersByTokens: ", data.data);
+      if (data.data.success) {
+        setLeaderboardHottestNewcomers(data.data.data);
+      } else {
+        setLeaderboardHottestNewcomers(data.data.message);
+      }
+    } catch (err) {
+      setLeaderboardHottestNewcomers("Error");
+      console.error(err);
+    }
+  };
+
+  const getAffLeadersMonthlyMovers = async (limit = 10, days = 30) => {
+    try {
+      const data = await marketPlaceInstance().get(
+        `/getAffLeadersByTokens/${limit}/${days}`
+      );
+      if (data.data.success) {
+        setLeaderboardMonthlyMovers(data.data.data);
+      } else {
+        setLeaderboardMonthlyMovers(data.data.message);
+      }
+    } catch (err) {
+      setLeaderboardMonthlyMovers("Error");
+      console.error(err);
+    }
+  };
+
+  async function getAffLeadersTopSales(type = "order", limit = 10, days = 0) {
+    try {
+      marketPlaceInstance()
+        .get(`/getAffLeadersByType/${type}/${limit}/${days}`)
+        .then((data) => {
+          //console.log("getAffLeadersByTokens: ", data.data);
+          if (typeof data.data !== "string") setLeaderboardTopSales(data.data);
+          return data.data;
+        });
+    } catch (err) {
+      console.error(err);
     }
   }
 
-    async function getAffLeadersByTokens(limit=10, days=0) {
-        try {
-            marketPlaceInstance().get(`/getAffLeadersByTokens/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                if(typeof data.data !== "string")
-                setLeaderboardByTokens(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
-    }
-  }
-
-    async function getAffLeadersHottestNewcomers(limit=10, days=7) {
-        try {
-            marketPlaceInstance().get(`/getAffLeadersByTokens/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                if(typeof data.data !== "string")
-                setLeaderboardHottestNewcomers(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
-    }
-  }
-
-    async function getAffLeadersMonthlyMovers(limit=10, days=30) {
-        try {
-            marketPlaceInstance().get(`/getAffLeadersByTokens/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                if(typeof data.data !== "string")
-                setLeaderboardMonthlyMovers(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
+  async function getAffLeadersTopRegisters(
+    type = "register",
+    limit = 10,
+    days = 0
+  ) {
+    try {
+      const data = await marketPlaceInstance().get(
+        `/getAffLeadersByType/${type}/${limit}/${days}`
+      );
+      //console.log("getAffLeadersByTokens: ", data.data);
+      if (data.data.success) {
+        setLeaderboardTopRegisters(data.data.data);
+      } else {
+        setLeaderboardTopRegisters(data.data.message);
+      }
+    } catch (err) {
+      setLeaderboardTopRegisters("Error");
+      console.error(err);
     }
   }
 
   useEffect(() => {
     //checkToken();
-    getAffLeadersByCount();
-    getAffLeadersByTokens();
-    getAffLeadersHottestNewcomers();
-    getAffLeadersMonthlyMovers();
-    getAffLeadersTopRegisters();
-    getAffLeadersTopSales();
+    const getData = async () => {
+      await getAffLeadersByCount();
+      await getAffLeadersByTokens();
+      await getAffLeadersHottestNewcomers();
+      await getAffLeadersMonthlyMovers();
+      await getAffLeadersTopRegisters();
+      // getAffLeadersTopSales();
+    };
+    getData();
   }, []);
 
-
-
-    async function getAffLeadersTopSales(type='order', limit=10, days=0) {
-        try {
-            marketPlaceInstance().get(`/getAffLeadersByType/${type}/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                if(typeof data.data !== "string")
-                setLeaderboardTopSales(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
-    }
-
-    async function getAffLeadersTopRegisters(type='register', limit=10, days=0) {
-        try {
-            marketPlaceInstance().get(`/getAffLeadersByType/${type}/${limit}/${days}`).then((data)=>{
-                //console.log("getAffLeadersByTokens: ", data.data);
-                if(typeof data.data !== "string")
-                setLeaderboardTopRegisters(data.data);
-                return data.data;
-            });
-        } catch (err) {
-            console.error(err);
-        };
-    }
-
-    
-    
-    useEffect(() => {
-        //checkToken();
-        getAffLeadersByCount();
-        getAffLeadersByTokens();
-        getAffLeadersHottestNewcomers();
-        getAffLeadersMonthlyMovers();
-        getAffLeadersTopRegisters();
-        getAffLeadersTopSales();
-      }, []);
-
-
-    return (<>
-    {(showLeaderboards)?(
-
-    
-    <div className="bordered-section">
-        <div className="flex-row-no-margin">
-            {(leaderboardByCount.length > 0)?(<>
+  return (
+    <>
+      {showLeaderboards ? (
+        <div className='bordered-section'>
+          <div className='flex-row-no-margin'>
+            {leaderboardByCount !== "Error" ? (
+              <>
                 <div className='leaderboard-card-div'>
-                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                  <div className='pageTitle' style={{ marginBottom: "20px" }}>
                     <h1>Affiliate Sales Leaders</h1>
                   </div>
-                  {leaderboardByCount.map((lead, index) => (
-                    <div className='leaderboard-row' key={lead._id}>
-                      <div>{index + 1}.</div>
-                      <div>Affiliate ID: {lead._id}</div>
-                      <div># of Sales: {lead.count}</div>
+                  {leaderboardByCount !== "No Entries Found For User" &&
+                  leaderboardByCount !== "Error in Request Process" &&
+                  leaderboardByCount.length ? (
+                    leaderboardByCount.map((lead, index) => (
+                      <div className='leaderboard-row' key={lead._id}>
+                        <div>{index + 1}.</div>
+                        <div>Affiliate ID: {lead._id}</div>
+                        <div># of Sales: {lead.count}</div>
+                      </div>
+                    ))
+                  ) : (
+                    <div>
+                      <div className='leaderboard-row'>
+                        <div>No Data Found</div>
+                      </div>
                     </div>
-                  ))}
+                  )}
                 </div>
               </>
             ) : (
               <></>
             )}
 
-            {leaderboardByTokens.length > 0 ? (
+            {leaderboardByTokens !== "Error" ? (
               <>
                 <div className='leaderboard-card-div'>
-                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                  <div className='pageTitle' style={{ marginBottom: "20px" }}>
                     <h1>Affiliate Tokens Leaders</h1>
                   </div>
-                  {leaderboardByTokens.map((lead, index) => (
-                    <div className='leaderboard-row' key={lead._id}>
-                      <div>{index + 1}.</div>
-                      <div>Affiliate ID: {lead._id}</div>
-                      <div>Earned: {lead.totalCommission} Tokens</div>
-                    </div>
-                  ))}
+                  {leaderboardByTokens !== "No Entries Found For User" &&
+                  leaderboardByTokens !== "Error in Request Process" &&
+                  leaderboardByTokens.length ? (
+                    leaderboardByTokens.map((lead, index) => (
+                      <div className='leaderboard-row' key={lead._id}>
+                        <div>{index + 1}.</div>
+                        <div>Affiliate ID: {lead._id}</div>
+                        <div>Earned: {lead.totalCommission} Tokens</div>
+                      </div>
+                    ))
+                  ) : (
+                    <div className='leaderboard-row'>No Data Found</div>
+                  )}
                 </div>
               </>
             ) : (
               <></>
             )}
 
-            {leaderboardHottestNewcomers.length > 0 ? (
+            {leaderboardHottestNewcomers !== "Error" ? (
               <>
                 <div className='leaderboard-card-div'>
-                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                  <div className='pageTitle' style={{ marginBottom: "20px" }}>
                     <h1>Hottest Newcomers</h1>
                   </div>
-                  {leaderboardHottestNewcomers.map((lead, index) => (
-                    <div className='leaderboard-row' key={lead._id}>
-                      <div>{index + 1}.</div>
-                      <div>Affiliate ID: {lead._id}</div>
-                      <div>Earned: {lead.totalCommission} Tokens</div>
-                    </div>
-                  ))}
+                  {leaderboardHottestNewcomers !==
+                    "No Entries Found For User" &&
+                  leaderboardHottestNewcomers !== "Error in Request Process" &&
+                  leaderboardHottestNewcomers.length ? (
+                    leaderboardHottestNewcomers.map((lead, index) => (
+                      <div className='leaderboard-row' key={lead._id}>
+                        <div>{index + 1}.</div>
+                        <div>Affiliate ID: {lead._id}</div>
+                        <div>Earned: {lead.totalCommission} Tokens</div>
+                      </div>
+                    ))
+                  ) : (
+                    <div className='leaderboard-row'>No Data Found</div>
+                  )}
                 </div>
               </>
             ) : (
               <></>
             )}
 
-            {leaderboardMonthlyMovers.length > 0 ? (
+            {leaderboardMonthlyMovers !== "Error" ? (
               <>
                 <div className='leaderboard-card-div'>
-                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                  <div className='pageTitle' style={{ marginBottom: "20px" }}>
                     <h1>Monthly Movers</h1>
                   </div>
-                  {leaderboardMonthlyMovers.map((lead, index) => (
-                    <div className='leaderboard-row' key={lead._id}>
-                      <div>{index + 1}.</div>
-                      <div>Affiliate ID: {lead._id}</div>
-                      <div>Earned: {lead.totalCommission} Tokens</div>
-                    </div>
-                  ))}
+                  {leaderboardMonthlyMovers !== "No Entries Found For User" &&
+                  leaderboardMonthlyMovers !== "Error in Request Process" &&
+                  leaderboardMonthlyMovers.length ? (
+                    leaderboardMonthlyMovers.map((lead, index) => (
+                      <div className='leaderboard-row' key={lead._id}>
+                        <div>{index + 1}.</div>
+                        <div>Affiliate ID: {lead._id}</div>
+                        <div>Earned: {lead.totalCommission} Tokens</div>
+                      </div>
+                    ))
+                  ) : (
+                    <div className='leaderboard-row'>No Data Found</div>
+                  )}
                 </div>
               </>
             ) : (
               <></>
             )}
 
-            {leaderboardTopRegisters.length > 0 ? (
+            {leaderboardTopRegisters !== "Error" ? (
               <>
                 <div className='leaderboard-card-div'>
-                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                  <div className='pageTitle' style={{ marginBottom: "20px" }}>
                     <h1>Top User Referrals</h1>
                   </div>
-                  {leaderboardTopRegisters.map((lead, index) => (
-                    <div className='leaderboard-row' key={lead._id}>
-                      <div>{index + 1}.</div>
-                      <div>Affiliate ID: {lead._id}</div>
-                      <div>Earned: {lead.totalCommission} Tokens</div>
-                    </div>
-                  ))}
+                  {leaderboardTopRegisters !== "No Entries Found For User" &&
+                  leaderboardTopRegisters !== "Error in Request Process" &&
+                  leaderboardTopRegisters.length ? (
+                    leaderboardTopRegisters.map((lead, index) => (
+                      <div className='leaderboard-row' key={lead._id}>
+                        <div>{index + 1}.</div>
+                        <div>Affiliate ID: {lead._id}</div>
+                        <div>Earned: {lead.totalCommission} Tokens</div>
+                      </div>
+                    ))
+                  ) : (
+                    <div className='leaderboard-row'>No Data Found</div>
+                  )}
                 </div>
               </>
             ) : (
               <></>
             )}
 
-            {leaderboardTopSales.length > 0 ? (
+            {leaderboardTopSales !== "Error" ? (
               <>
                 <div className='leaderboard-card-div'>
-                  <div className='pageTitle' style={{ marginBottom: '20px' }}>
+                  <div className='pageTitle' style={{ marginBottom: "20px" }}>
                     <h1>Top Product Sales</h1>
                   </div>
-                  {leaderboardTopSales.map((lead, index) => (
-                    <div className='leaderboard-row' key={lead._id}>
-                      <div>{index + 1}.</div>
-                      <div>Affiliate ID: {lead._id}</div>
-                      <div>Earned: {lead.totalCommission} Tokens</div>
-                    </div>
-                  ))}
+                  {leaderboardTopSales !== "No Entries Found For User" &&
+                  leaderboardTopSales !== "Error in Request Process" &&
+                  leaderboardTopSales.length ? (
+                    leaderboardTopSales.map((lead, index) => (
+                      <div className='leaderboard-row' key={lead._id}>
+                        <div>{index + 1}.</div>
+                        <div>Affiliate ID: {lead._id}</div>
+                        <div>Earned: {lead.totalCommission} Tokens</div>
+                      </div>
+                    ))
+                  ) : (
+                    <div className='leaderboard-row'>No Data Found</div>
+                  )}
                 </div>
               </>
             ) : (
