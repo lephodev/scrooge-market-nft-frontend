947930433333cc32bcc87b6cc544297f6c54ff71
RizwanAhmad-dev <rizwan@websultanate.com>
2023-03-31 15:10:51 +0530

    added condition for social sharing

--- src/components/SharableData.mjs
+++ src/components/SharableData.mjs
@@ -237,7 +237,7 @@ export default function SharableData() {
             ) : (
               <></>
             )}
-            {!showSocialShare ? (
+            {!showSocialShare  && affUser?.message?.toString() !== "User not found."? (
               <>
                 <div className='new-btn'>
                   <button
