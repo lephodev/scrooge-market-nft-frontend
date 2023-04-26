45483140c2b0e210bea2285b4432a8afc156376c
RizwanAhmad-Dev <rizwan@websultanate.com>
2023-04-26 19:11:18 +0530

    combine daily monthly rewards

--- src/components/SharableData.mjs
+++ src/components/SharableData.mjs
@@ -8,7 +8,6 @@ import "react-toastify/dist/ReactToastify.css";
 import LoadingPoker from "../images/scroogeHatLogo.png";
 import DiceGif from "../images/diceGif.gif";
 import AffiliateLeaderboard from "../components/AffiliateLeaderboard.mjs";
-import DailyRewards from "../components/DailyRewards.mjs";
 import profile from "../images/profile.png";
 import {
   EmailShareButton,
@@ -430,23 +429,6 @@ const handleShareError = () => {
       ) : (
         <></>
       )}
-      {console.log(affUser?.success)}
-      {user && affUser?.success && showDailyRewards ? (
-        <>
-          <div className='close-btn-round-div'>
-            <div
-              className='close-btn-round'
-              style={{ width: "45px", marginTop: "0" }}
-              onClick={() => setShowDailyRewards(false)}>
-              X
-            </div>
-          </div>
-          <DailyRewards />
-        </>
-      ) : (
-        <></>
-      )}
-
       {showAffLeaderboards ? (
         <>
           <div className='close-btn-round-div'>
