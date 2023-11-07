869e0ea4a7df3f43f7c943d09dc1a24de62611c6
jivan-deep <jivan@websultanate.com>
2023-11-07 13:33:10 +0530

    added data

--- src/pages/models/SuccessModal.mjs
+++ src/pages/models/SuccessModal.mjs
@@ -1,21 +1,61 @@
 import { Modal } from "react-bootstrap";
 import redemption50 from "../../images/succes/redemption-50.webp";
+import redemption100 from "../../images/succes/redemption-100.webp";
+import redemption500 from "../../images/succes/redemption-500.webp";
 
-const SuccessModal = ({ successShow, handleSuccessModal }) => {
+const SuccessModal = ({
+  success50Show,
+  success100Show,
+  success500Show,
+  handleSuccess500Modal,
+  handleSuccess50Modal,
+  handleSuccess100Modal,
+}) => {
   return (
-    <Modal
-      centered
-      show={successShow}
-      onHide={handleSuccessModal}
-      size="lg"
-      className="success-modal"
-    >
-      <Modal.Body>
-        <div className="success-modal-content">
-          <img src={redemption50} alt="redemption" className="img-fluid" />
-        </div>
-      </Modal.Body>
-    </Modal>
+    <>
+      {success50Show ? (
+        <Modal
+          centered
+          show={success50Show}
+          onHide={handleSuccess50Modal}
+          size='lg'
+          className='success-modal'>
+          <Modal.Body>
+            <div className='success-modal-content'>
+              <img src={redemption50} alt='redemption' className='img-fluid' />
+            </div>
+          </Modal.Body>
+        </Modal>
+      ) : success100Show ? (
+        <Modal
+          centered
+          show={success100Show}
+          onHide={handleSuccess100Modal}
+          size='lg'
+          className='success-modal'>
+          <Modal.Body>
+            <div className='success-modal-content'>
+              <img src={redemption100} alt='redemption' className='img-fluid' />
+            </div>
+          </Modal.Body>
+        </Modal>
+      ) : success500Show ? (
+        <Modal
+          centered
+          show={success500Show}
+          onHide={handleSuccess500Modal}
+          size='lg'
+          className='success-modal'>
+          <Modal.Body>
+            <div className='success-modal-content'>
+              <img src={redemption500} alt='redemption' className='img-fluid' />
+            </div>
+          </Modal.Body>
+        </Modal>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
