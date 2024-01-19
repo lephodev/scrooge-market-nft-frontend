c9aa87ca3bde46f9508b45abc16fd22323ab7627
jivan-deep <jivan@websultanate.com>
2024-01-19 13:11:50 +0530

    added toast meassage for unsupported images

--- src/utils/api.mjs
+++ src/utils/api.mjs
@@ -1,33 +1,34 @@
 import { kycInstance } from "../config/axios.js";
 
 export const createKYC = async (requestData) => {
-    try {
-      const res= await kycInstance().post(`users/createKyc`, requestData);
-      return res
-    } catch (error) {
-      console.log('Error in KYC brands api =>', error);
-      return error.response.data;
-    }
-  };
+  try {
+    const res = await kycInstance().post(`users/createKyc`, requestData);
+    return res;
+  } catch (error) {
+    console.log("Error in KYC brands api =>", error);
 
-  export const userKycDetails = async () => {
-    try {
-      const response = await kycInstance().get(`users/getKycDetails`);
-      console.info('get Kyc Details', response);
-      return response.data;
-    } catch (error) {
-      console.info('error in deposit>>', error);
-      return error?.response?.data;
-    }
-  };
+    return error.response.data;
+  }
+};
 
-  export const reApply = async () => {
-    try {
-      const response = await kycInstance().get(`users/reApply`);
-      console.info('get Kyc Details', response);
-      return response.data;
-    } catch (error) {
-      console.info('error in deposit>>', error);
-      return error?.response?.data;
-    }
-  };
+export const userKycDetails = async () => {
+  try {
+    const response = await kycInstance().get(`users/getKycDetails`);
+    console.info("get Kyc Details", response);
+    return response.data;
+  } catch (error) {
+    console.info("error in deposit>>", error);
+    return error?.response?.data;
+  }
+};
+
+export const reApply = async () => {
+  try {
+    const response = await kycInstance().get(`users/reApply`);
+    console.info("get Kyc Details", response);
+    return response.data;
+  } catch (error) {
+    console.info("error in deposit>>", error);
+    return error?.response?.data;
+  }
+};
