7d90502c043d26f56c6351b4d72ccc2a1d252a45
jivan-deep <jivan@websultanate.com>
2024-03-18 10:03:19 +0530

    Added taost message on spending limit

--- src/pages/CryptoToGC.mjs
+++ src/pages/CryptoToGC.mjs
@@ -1156,7 +1156,7 @@ const PayWithCard = ({
   const [loader, setLoading] = useState(false);
 
   const handleCLick = async (gc, usd) => {
-    console.log("dailyGCPurchaseLimit",dailyGCPurchaseLimit);
+    console.log("dailyGCPurchaseLimit", dailyGCPurchaseLimit);
     if (dailyGCPurchaseLimit >= 4) {
       return toast.error("Credit card daily purchase limit are reached");
     }
@@ -1226,27 +1226,45 @@ const PayWithCard = ({
     }
   }, [liveFormToken]);
 
-  const handleLimitCheck=(usd)=>{
-    if(spendedAmount.spneded_this_month + usd >
-      user.monthlyGoldCoinSpendingLimit){
-        console.log("spendedAmount.spneded_this_month",spendedAmount.spneded_this_month, user.monthlyGoldCoinSpendingLimit,usd);
+  const handleLimitCheck = (usd) => {
+    if (
+      spendedAmount.spneded_this_month + usd >
+      user.monthlyGoldCoinSpendingLimit
+    ) {
+      console.log(
+        "spendedAmount.spneded_this_month",
+        spendedAmount.spneded_this_month,
+        user.monthlyGoldCoinSpendingLimit,
+        usd
+      );
 
-    return true
-      }
-      if(spendedAmount.spened_this_week + usd >
-        user.weeklyGoldCoinSpendingLimit){
-          console.log("spendedAmount.spneded_this_week",spendedAmount.spened_this_week,
-          user.weeklyGoldCoinSpendingLimit,usd);
+      return true;
+    }
+    if (
+      spendedAmount.spened_this_week + usd >
+      user.weeklyGoldCoinSpendingLimit
+    ) {
+      console.log(
+        "spendedAmount.spneded_this_week",
+        spendedAmount.spened_this_week,
+        user.weeklyGoldCoinSpendingLimit,
+        usd
+      );
 
-      return true
-        }
-        if(spendedAmount.spended_today + usd > user.dailyGoldCoinSpendingLimit){
-          console.log("spendedAmount.spneded_this_day",spendedAmount.spended_today,user.dailyGoldCoinSpendingLimit,usd)
-        return true
-          }
+      return true;
+    }
+    if (spendedAmount.spended_today + usd > user.dailyGoldCoinSpendingLimit) {
+      console.log(
+        "spendedAmount.spneded_this_day",
+        spendedAmount.spended_today,
+        user.dailyGoldCoinSpendingLimit,
+        usd
+      );
+      return true;
+    }
 
     return false;
-  }
+  };
 
   return (
     <>
@@ -1262,8 +1280,12 @@ const PayWithCard = ({
         </button>
       ) : (
         <button
-        className={handleLimitCheck(parseFloat(prize?.priceInBUSD))?"disable-btn-purchase":""}
-        disabled={handleLimitCheck(parseFloat(prize?.priceInBUSD))}
+          className={
+            handleLimitCheck(parseFloat(prize?.priceInBUSD))
+              ? "disable-btn-purchase"
+              : ""
+          }
+          // disabled={handleLimitCheck(parseFloat(prize?.priceInBUSD))}
           onClick={() =>
             handleCLick(prize?.gcAmount, parseFloat(prize?.priceInBUSD))
           }
