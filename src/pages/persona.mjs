53efe393a7016b3aae402bb9dd64f96110cbd1f6
jivan-deep <jivan@websultanate.com>
2024-06-06 16:28:34 +0530

    added checks

--- src/pages/persona.mjs
+++ src/pages/persona.mjs
@@ -1,11 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import Persona from "persona";
 import { Button } from "react-bootstrap";
 import AuthContext from "../context/authContext.ts";
 
 const PersonaComponent = ({ phoneNum, errors }) => {
-  const [client, setClient] = useState(null);
   const { user } = useContext(AuthContext);
   console.log("user", user);
   const {
@@ -20,9 +19,47 @@ const PersonaComponent = ({ phoneNum, errors }) => {
     id,
   } = user;
 
-  useEffect(() => {
-    // Initialize the Persona client and store it in state
+  // useEffect(() => {
+  //   // Initialize the Persona client and store it in state
+  //   if (Object.keys(errors).length === 0 && phoneNum?.length > 9) {
+  //     const personaClient = new Persona.Client({
+  //       templateId: "itmpl_CZuaAe3eTnfzoQ4qp1Z2yHMJAqeH", // Your template ID
+  //       environmentId: "env_qRUUMWexbxJxkuPhq7eRsEZvMgVw",
+  //       referenceId: _id || id,
+  //       fields: {
+  //         nameFirst: firstName,
+  //         nameLast: lastName,
+  //         birthdate: birthDate,
+  //         addressStreet1: address,
+  //         addressCity: city,
+  //         addressPostalCode: zipCode,
+  //         addressCountryCode: "US",
+  //         phoneNumber: phoneNum,
+  //         emailAddress: email,
+  //         // customAttribute: "hello",
+  //       },
+  //       onReady: () => {
+  //         console.log("Persona client is ready");
+  //       },
+  //       onComplete: ({ inquiryId, status, fields }) => {
+  //         console.log("onComplete", inquiryId, status, fields);
+  //         window.location.href = "/kyc";
+  //       },
+  //       onCancel: ({ inquiryId, sessionToken }) => {
+  //         console.log("onCancel", inquiryId, sessionToken);
+  //       },
+  //       onError: (error) => {
+  //         console.error("onError", error);
+  //       },
+  //     });
+  //     setClient(personaClient);
+  //   }
+  // }, [phoneNum]);
+
+  const handleOpenPersona = () => {
+    console.log("phoneNumber", phoneNum);
     if (Object.keys(errors).length === 0 && phoneNum?.length > 9) {
+      // if (client) {
       const personaClient = new Persona.Client({
         templateId: "itmpl_CZuaAe3eTnfzoQ4qp1Z2yHMJAqeH", // Your template ID
         environmentId: "env_qRUUMWexbxJxkuPhq7eRsEZvMgVw",
@@ -53,18 +90,10 @@ const PersonaComponent = ({ phoneNum, errors }) => {
           console.error("onError", error);
         },
       });
-      setClient(personaClient);
-    }
-  }, [phoneNum]);
-
-  const handleOpenPersona = () => {
-    console.log("phoneNumber", phoneNum);
-    if (Object.keys(errors).length === 0 && phoneNum?.length > 9) {
-      if (client) {
-        client.open();
-      } else {
-        console.error("Persona client is not initialized");
-      }
+      personaClient.open();
+      // } else {
+      //   console.error("Persona client is not initialized");
+      // }
     }
   };
 
