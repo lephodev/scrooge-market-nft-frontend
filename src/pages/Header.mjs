de9bf197569f63765117848a2b711d8041102218
vipin <vipin.v@websultanate.com>
2024-07-16 12:54:33 +0530

    sidebar menu design completed

--- src/pages/Header.mjs
+++ src/pages/Header.mjs
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import newLogo from "../images/new-logo.webp";
 import { Button, Dropdown, Form, Nav, Navbar } from "react-bootstrap";
 import { FaPlusCircle } from "react-icons/fa";
 import cookie from "js-cookie";
-
+import { HiOutlineLogout } from "react-icons/hi";
 import {
   // blackjackUrl,
   // pokerUrl,
@@ -11,6 +11,8 @@ import {
   scroogeClient,
   // slotUrl,
 } from "../config/keys.js";
+
+// import playPolicy from "../asset/SCROOGE CASINO Game Play Policy.docx";
 import bell from "../images/bell.svg";
 import profile from "../images/profile.png";
 import { Link, useLocation } from "react-router-dom";
@@ -19,6 +21,8 @@ import AuthContext from "../context/authContext.ts";
 import { authInstance } from "../config/axios.js";
 const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [isDropdownVisible, setDropdownVisible] = useState("");
+  const subMenuRef = useRef();
   const { mode, user, setMode } = useContext(AuthContext);
   const useCurrentPath = () => {
     const location = useLocation();
@@ -67,6 +71,21 @@ const Header = () => {
 
   const currentRoute = useCurrentPath();
   const isActive = (routeName) => (routeName === currentRoute ? "active" : "");
+   
+  const handleMouseEnter = (value) => {
+    setDropdownVisible(value);
+  };
+  const handleMouseLeave = () => {
+    setDropdownVisible("");
+  };
+  const handleDropDown = (value) => {
+    if (value === isDropdownVisible) {
+      setDropdownVisible(" ");
+      return;
+    }
+    setDropdownVisible(value);
+  };
+
   return (
     // <div className='headerContainer'>
     //   <div className='header_top'>
@@ -226,6 +245,7 @@ const Header = () => {
 
               <Nav className="mr-auto ">
                 {user ? (
+                  <>
                   <div className="navItem">
                     <Link
                       to={scroogeClient}
@@ -251,7 +271,7 @@ const Header = () => {
                       to={`/redeem-prizes`}
                       className={`nav-link ${isActive("/redeem-prizes")}`}
                     >
-                      Redemption Center
+                      Redemption Center 
                     </Link>
                     <Link
                       to={`/claim-free-tokens`}
@@ -272,6 +292,147 @@ const Header = () => {
                       Holder Claim Center
                     </Link> */}
                   </div>
+                  <div className="header-btn-mobile user-profile">
+                        <a
+                          className="dropdown-item"
+                          href={`${scroogeClient}/profile`}
+                        >
+                          <img
+                            src={user?.profile ? user?.profile : profile}
+                            alt="profile"
+                            onError={(e) => {
+                              e.target.onerror = null;
+                              e.target.src = profile;
+                            }}
+                          />
+                        </a>
+                        <a
+                          className="dropdown-item text-white"
+                          href={`${scroogeClient}/setting`}
+                        >
+                          <SettingSvg /> 
+                        </a>
+                        {/* <div className="header-btn-mobile logout-btn">
+                        <Button onClick={handleLogout}>
+                          <HiOutlineLogout />
+                        </Button>
+                      </div> */}
+                      </div>
+
+                      <div className="header-btn-mobile user-profile policy-menu">
+                              <div
+                                className={`nav-link custom-dd ${isActive(
+                                  "/redeem-prizes"
+                                )}`}
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                                onClick={() => {
+                                  handleDropDown("responsible");
+                                }}
+                                role="presentation"
+                                // ref={subMenuRef}
+                              >
+                                <span
+                                  className={`${
+                                    isDropdownVisible !== "responsible"
+                                      ? "drop-arrow"
+                                      : ""
+                                  }`}
+                                >
+                                  Responsible Gaming Controls{" "}
+                                  {/* <i className="las la-angle-down" /> */}
+                                  <ArrowIcon />
+                                </span>
+                                {isDropdownVisible === "responsible" ? (
+                                  <div className="custom-dropdown-menu">
+                                    <ul>
+                                      <li>
+                                        <Link to="/profile?active=spending">
+                                          {" "}
+                                          Spending Limit
+                                        </Link>
+                                      </li>
+                                      <li>
+                                        <Link to="/deactivateAccount">
+                                          {" "}
+                                          Access Restrictions
+                                        </Link>
+                                      </li>
+                                      <li>
+                                        <Link to="/profile?active=transaction">
+                                          {" "}
+                                          Account History
+                                        </Link>
+                                      </li>
+                                    </ul>
+                                  </div>
+                                  ): null} 
+                              </div>
+                            </div>
+
+                            <div className="header-btn-mobile user-profile policy-menu">
+                              <div
+                                className={`nav-link custom-dd ${isActive(
+                                  "/redeem-prizes"
+                                )}`}
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                                onClick={() => {
+                                  handleDropDown("terms");
+                                }}
+                                role="presentation"
+                                ref={subMenuRef}
+                              >
+                                <span
+                                  className={`${
+                                    isDropdownVisible !== "terms"
+                                      ? "drop-arrow"
+                                      : ""
+                                  }`}
+                                >
+                                  Terms and Policies{" "}
+                                  {/* <i className="las la-angle-down" /> */}
+                                  <ArrowIcon />
+                                </span>
+                                {isDropdownVisible === "terms" ? (
+                                  <div className="custom-dropdown-menu">
+                                    <ul>
+                                      <li>
+                                        <Link to="/privacyPolicy">
+                                          Privacy policy
+                                        </Link>
+                                      </li>
+                                      <li>
+                                        <Link to="/termsncondition">
+                                          Terms of Services
+                                        </Link>
+                                      </li>
+                                      <li>
+                                        {" "}
+                                        <a href={"/"} target="blank">
+                                          Responsible Social Gameplay
+                                        </a>
+                                      </li>
+                                      <li>
+                                        <Link to="/sweepsrules" target="blank">
+                                          Sweeps Rules
+                                        </Link>
+                                      </li>
+                                    </ul>
+                                  </div>
+                                ) : null}
+                              </div>
+                            </div>
+
+                            <div className="header-btn-mobile user-profile side-logout">
+                              <Button
+                                onClick={handleLogout}
+                                // disabled={disableRedirections}
+                              >
+                                Logout <HiOutlineLogout width={100} />
+                              </Button>
+                            </div>
+                  </>
                 ) : (
                   <a
                     href={`${scroogeClient}`}
@@ -293,3 +454,25 @@ const Header = () => {
 };
 
 export default Header;
+
+function SettingSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      width="50px"
+      height="50px"
+    >
+      {" "}
+      <path d="M47.16,21.221l-5.91-0.966c-0.346-1.186-0.819-2.326-1.411-3.405l3.45-4.917c0.279-0.397,0.231-0.938-0.112-1.282 l-3.889-3.887c-0.347-0.346-0.893-0.391-1.291-0.104l-4.843,3.481c-1.089-0.602-2.239-1.08-3.432-1.427l-1.031-5.886 C28.607,2.35,28.192,2,27.706,2h-5.5c-0.49,0-0.908,0.355-0.987,0.839l-0.956,5.854c-1.2,0.345-2.352,0.818-3.437,1.412l-4.83-3.45 c-0.399-0.285-0.942-0.239-1.289,0.106L6.82,10.648c-0.343,0.343-0.391,0.883-0.112,1.28l3.399,4.863 c-0.605,1.095-1.087,2.254-1.438,3.46l-5.831,0.971c-0.482,0.08-0.836,0.498-0.836,0.986v5.5c0,0.485,0.348,0.9,0.825,0.985 l5.831,1.034c0.349,1.203,0.831,2.362,1.438,3.46l-3.441,4.813c-0.284,0.397-0.239,0.942,0.106,1.289l3.888,3.891 c0.343,0.343,0.884,0.391,1.281,0.112l4.87-3.411c1.093,0.601,2.248,1.078,3.445,1.424l0.976,5.861C21.3,47.647,21.717,48,22.206,48 h5.5c0.485,0,0.9-0.348,0.984-0.825l1.045-5.89c1.199-0.353,2.348-0.833,3.43-1.435l4.905,3.441 c0.398,0.281,0.938,0.232,1.282-0.111l3.888-3.891c0.346-0.347,0.391-0.894,0.104-1.292l-3.498-4.857 c0.593-1.08,1.064-2.222,1.407-3.408l5.918-1.039c0.479-0.084,0.827-0.5,0.827-0.985v-5.5C47.999,21.718,47.644,21.3,47.16,21.221z M25,32c-3.866,0-7-3.134-7-7c0-3.866,3.134-7,7-7s7,3.134,7,7C32,28.866,28.866,32,25,32z" />
+    </svg>
+  );
+}
+
+function ArrowIcon() {
+  return(
+    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 1L5 5L9 1" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+  )
+}
