e57fe3c3f5f1233ec3563b433e1c5a66e5d37492
jivan-deep <jivan@websultanate.com>
2024-10-25 15:43:05 +0530

    added 2fa

---
+++ src/pages/twofa/PopUp2FA.mjs
@@ -0,0 +1,99 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { useState } from "react";
+// import { Transition } from "react-transition-group";
+import { Button, Modal } from "react-bootstrap";
+// import '../../components/Affiliates/re'
+import "./twofa.css";
+
+import "react-toastify/dist/ReactToastify.css";
+import OTPInput from "react-otp-input";
+
+function PopUp2FA({
+  displayData,
+  handleClick,
+  twoFactorData,
+  from,
+  handleLostLine,
+  handleClose,
+}) {
+  const [otp, setOtp] = useState("");
+
+  const handleChange = (value) => {
+    setOtp(value);
+    console.log("value", value.length);
+    if (value.length === 6 && from === "header") {
+      console.log("called");
+      handleClick(value);
+    }
+  };
+
+  console.log(
+    "twoFactorData?.twoFactorAuthEnabled",
+    twoFactorData?.twoFactorAuthEnabled
+  );
+
+  return (
+    <Modal
+      className="welcomeModal twofa-popup-qr-model"
+      centered
+      size="lg"
+      show
+      // onHide={handleCloseLogin}
+    >
+      <Modal.Body>
+        <div className="loginPopup">
+          <div className="twofaPopupForm">
+            {twoFactorData?.twoFactorAuthEnabled === true ? (
+              <div className="twofaPopupForm-QR">
+                <p className="mt-[0px] mb-[10px] remove2fa_para">
+                  {displayData.title || ""}
+                </p>
+              </div>
+            ) : (
+              <div className="twofaPopupForm-QR">
+                <img src={twoFactorData?.qrImage} />
+                <p className="mt-[0px] mb-[10px]">
+                  {twoFactorData?.tempTwoFactorSecret || ""}
+                </p>
+              </div>
+            )}
+
+            <div className="mb-[10px]">
+              <p className="text-center text-[13px] text-[#aeafae]">
+                {displayData?.desc}
+              </p>
+            </div>
+
+            <div className="">
+              <h6 className="">{displayData?.tagLine}</h6>
+
+              <OTPInput
+                value={otp}
+                onChange={handleChange}
+                numInputs={6}
+                renderSeparator={<span>-</span>}
+                renderInput={(props) => <input {...props} />}
+              />
+
+              {/* <div className="enable-2fabtn-second">
+                {displayData?.lostline ? (
+                  <Button onClick={handleLostLine}>
+                    {displayData?.lostline}
+                  </Button>
+                ) : null}
+              </div> */}
+
+              <div className="enable-2fabtn">
+                <Button onClick={handleClose}>
+                  {displayData?.nextBtnText}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default PopUp2FA;
