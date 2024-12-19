f38db47fb289798b0d2a972af35c1ab5641de3c3
jivan-deep <jivan@websultanate.com>
2024-12-19 10:16:53 +0530

    added datadog

--- src/index.tsx
+++ src/index.tsx
@@ -318,24 +318,27 @@ export default function App() {
 
 
   const handleDataDogRUM = () => {
-    datadogRum.init({
-      applicationId: "1f133214-5fde-4e92-814a-07adb090c9c4",
-      clientToken: "pubb242301634f196fc1901a52c385c616f",
-      // `site` refers to the Datadog site parameter of your organization
-      // see https://docs.datadoghq.com/getting_started/site/
-      site: "datadoghq.com",
-      service: "prod-web",
-      env: "prod-web",
-      // Specify a version number to identify the deployed version of your application in Datadog
-      // version: '1.0.0',
-      sessionSampleRate: 100,
-      sessionReplaySampleRate: 20,
-      trackUserInteractions: true,
-      trackResources: true,
-      trackLongTasks: true,
-      defaultPrivacyLevel: "mask-user-input",
-      allowedTracingUrls: [(url) => url.startsWith(server)],
-    });
+    const isInIt = datadogRum.getInternalContext();
+    if (!isInIt) {
+      datadogRum.init({
+        applicationId: "1f133214-5fde-4e92-814a-07adb090c9c4",
+        clientToken: "pubb242301634f196fc1901a52c385c616f",
+        // `site` refers to the Datadog site parameter of your organization
+        // see https://docs.datadoghq.com/getting_started/site/
+        site: "datadoghq.com",
+        service: "prod-web",
+        env: "prod-web",
+        // Specify a version number to identify the deployed version of your application in Datadog
+        // version: '1.0.0',
+        sessionSampleRate: 100,
+        sessionReplaySampleRate: 20,
+        trackUserInteractions: true,
+        trackResources: true,
+        trackLongTasks: true,
+        defaultPrivacyLevel: "mask-user-input",
+        allowedTracingUrls: [(url) => url.startsWith(server)],
+      });
+    }
   };
   if (true) {
     handleDataDogRUM();
